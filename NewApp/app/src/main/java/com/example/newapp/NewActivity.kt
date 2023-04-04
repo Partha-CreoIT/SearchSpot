e493069fa535e5703cb81f068e32d316cf259191
Partha S <partha@creoit.com>
2023-04-04 11:57:56 +0530

    Added a new Activity class and an activity layout file and showing a politics news in new activity

---
+++ NewApp/app/src/main/java/com/example/newapp/NewActivity.kt
@@ -0,0 +1,11 @@
+package com.example.newapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.politics_news_activity)
+    }
+}
