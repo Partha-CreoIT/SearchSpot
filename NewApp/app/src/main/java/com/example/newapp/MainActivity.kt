8d2a79f9fdc12f225edadb49b9796b3e80d5b517
Partha S <partha@creoit.com>
2023-04-04 14:58:29 +0530

    Initial Commit

--- NewApp/app/src/main/java/com/example/newapp/MainActivity.kt
+++ NewApp/app/src/main/java/com/example/newapp/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.newapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_acitivity)
+
+        val buttonClick = findViewById<Button>(R.id.button)
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, NewActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
